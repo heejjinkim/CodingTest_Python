76fa49bdb77267468f881ca9eaed2816a6346254
Heejin Kim <108065505+heejjinkim@users.noreply.github.com>
2023-09-20 21:03:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,3 @@
 def solution(s):
-    ar = sorted([int(n) for n in s.split(" ")])
-    return str(ar[0])+" "+str(ar[-1])
+    ar = sorted([int(n) for n in s.split(" ")]) #list(map(int, s.split()))
+    return str(ar[0])+" "+str(ar[-1]) #min, max 사용 가능
